221a0c9d02897dda3868a7248203b45333707f89
sparshturkane <rajaturkane99@gmail.com>
2017-09-04 14:47:52 +0530

    26 event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    selector: 'app-servers',
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    serverCreationStatus: string = 'no server is created';
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    onCreateServer() {
+        this.serverCreationStatus = 'server created';
+    }
 
 }
+
